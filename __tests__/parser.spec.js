28a67f8c26962b033f84e483e0a3b5c57475b2c3
Matthieu Oger <moger@pixelnest.io>
2016-12-17 01:54:57 +0100

    Rework parsing and loading of data files

--- __tests__/parser.spec.js
+++ __tests__/parser.spec.js
@@ -16,68 +16,42 @@ describe('XML Parser', () => {
   let companyXML = fs.readFileSync('./docs/data/company/data.xml', 'utf-8')
   let productXML = fs.readFileSync('./docs/data/product/data.xml', 'utf-8')
 
-  it('handles undefined XML strings', () => {
-    parser.parseXML(undefined, (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
-  })
-
-  it('handles null XML strings', () => {
-    parser.parseXML(null, (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
-  })
-
-  it('handles empty XML strings', () => {
-    parser.parseXML('', (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
+  it('handles empty, null or undefined XML strings', () => {
+    expect(() => parser.parseXML(undefined)).toThrow()
+    expect(() => parser.parseXML(null)).toThrow()
+    expect(() => parser.parseXML('')).toThrow()
   })
 
   it('handles invalid XML strings', () => {
-    parser.parseXML('Test. This is not XML', (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
+    expect(() => parser.parseXML('Test. This is not XML')).toThrow()
   })
 
-  it('handles incompleted XML strings', () => {
+  it('handles incomplete XML strings', () => {
     let data = '<?xml version="1.0"?><catalog><book id="bk101"><author>Gambardella, Matthew'
 
-    parser.parseXML(data, (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
+    expect(() => parser.parseXML(data)).toThrow()
   })
 
   it('handles valid XML strings but invalid presskit data', () => {
     let data = '<?xml version="1.0"?><catalog><book id="bk101"><author>Gambardella, Matthew</author><title>XML Developer\'s Guide</title><genre>Computer</genre><price>44.95</price><publish_date>2000-10-01</publish_date><description>An in-depth look at creating applications with XML.</description></book></catalog>'
 
-    parser.parseXML(data, (err, data) => {
-      expect(data).toBeUndefined()
-      expect(err).toBeDefined()
-    })
+    expect(() => parser.parseXML(data)).toThrow()
   })
 
-  it('handles valid XML company presskit string', () => {
-    parser.parseXML(companyXML, (err, data) => {
-      expect(err).toBeNull()
-      expect(data.type).toBe('company')
-      expect(data.title).toBeDefined()
-      expect(data.description).toBeDefined()
-    })
+  it('handles valid XML `company` presskit string', () => {
+    let result = parser.parseXML(companyXML)
+
+    expect(result.type).toBe('company')
+    expect(result.title).toBeDefined()
+    expect(result.description).toBeDefined()
   })
 
-  it('handles valid XML game presskit string', () => {
-    parser.parseXML(productXML, (err, data) => {
-      expect(err).toBeNull()
-      expect(data.type).toBe('product')
-      expect(data.title).toBeDefined()
-      expect(data.description).toBeDefined()
-    })
+  it('handles valid XML `product` or `game` presskit string', () => {
+    let result = parser.parseXML(productXML)
+
+    expect(result.type).toBe('product')
+    expect(result.title).toBeDefined()
+    expect(result.description).toBeDefined()
   })
 })
 
@@ -89,63 +63,37 @@ describe('JSON Parser', () => {
   let companyJSON = fs.readFileSync('./docs/data/company/data.json', 'utf-8')
   let productJSON = fs.readFileSync('./docs/data/product/data.json', 'utf-8')
 
-  it('handles undefined JSON strings', () => {
-    parser.parseJSON(undefined, (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
-  })
-
-  it('handles null JSON strings', () => {
-    parser.parseJSON(null, (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
-  })
-
-  it('handles empty JSON strings', () => {
-    parser.parseJSON('', (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
+  it('handles empty, null or undefined JSON strings', () => {
+    expect(() => parser.parseJSON(undefined)).toThrow()
+    expect(() => parser.parseJSON(null)).toThrow()
+    expect(() => parser.parseJSON('')).toThrow()
   })
 
   it('handles invalid JSON strings', () => {
-    parser.parseJSON('Test. This is not JSON', (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
+    expect(() => parser.parseJSON('Test. This is not JSON')).toThrow()
   })
 
-  it('handles incompleted JSON strings', () => {
-    parser.parseJSON('{ "game" : { "title": "My Super Game!!"', (err, data) => {
-      expect(err).toBeDefined()
-      expect(data).toBeUndefined()
-    })
+  it('handles incomplete JSON strings', () => {
+    expect(() => parser.parseJSON('{ "game" : { "title": "My Super Game!!"')).toThrow()
   })
 
   it('handles valid JSON strings but invalid presskit data', () => {
     let data = '{"employees":[{"firstName":"John","lastName":"Doe"},{"firstName":"Anna","lastName":"Smith"},{"firstName":"Peter","lastName":"Jones"}]}'
 
-    parser.parseJSON(data, (err, data) => {
-      expect(data).toBeUndefined()
-      expect(err).toBeDefined()
-    })
+    expect(() => parser.parseJSON(data)).toThrow()
   })
 
-  it('handles valid JSON company presskit string', () => {
-    parser.parseJSON(companyJSON, (err, data) => {
-      expect(err).toBeNull()
-      expect(data.type).toBe('company')
-      expect(data.title).toBeDefined()
-    })
+  it('handles valid JSON `company` presskit string', () => {
+    let result = parser.parseJSON(companyJSON)
+
+    expect(result.type).toBe('company')
+    expect(result.title).toBeDefined()
   })
 
-  it('handles valid JSON game presskit string', () => {
-    parser.parseJSON(productJSON, (err, data) => {
-      expect(err).toBeNull()
-      expect(data.type).toBe('product')
-      expect(data.title).toBeDefined()
-    })
+  it('handles valid JSON `product` or `game` presskit string', () => {
+    let result = parser.parseJSON(productJSON)
+
+    expect(result.type).toBe('product')
+    expect(result.title).toBeDefined()
   })
 })
