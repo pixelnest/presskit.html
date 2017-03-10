2d34d0404ccf2912a9bc925cdfe72fc94f47a7d4
Matthieu Oger <moger@pixelnest.io>
2017-03-10 14:59:01 +0100

    Add subcommand system

---
+++ bin/presskit-build.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const chalk = require('chalk')
+const program = require('commander')
+const presskit = require('../lib/index')
+
+// -------------------------------------------------------------
+// Constants.
+// -------------------------------------------------------------
+
+const version = require('../package.json').version
+
+const usage = chalk.green('[options]') + ' ' + chalk.yellow('<entry point>')
+
+const description = `Generate a "Presskit" based on information found in data.xml files. The format and the ouput are (nearly) the same as ${chalk.blue('http://dopresskit.com/')} by Rami Ismail (http://vlambeer.com). However, this command will generate static HTML files.
+
+  More information on ${chalk.blue('https://github.com/pixelnest/presskit.html')}`
+
+// -------------------------------------------------------------
+// Module.
+// -------------------------------------------------------------
+
+program
+  .version(version)
+  .description(description)
+  .usage(usage)
+  .option('-w, --watch', 'Watch project for changes and re-generate if needed')
+  .parse(process.argv)
+
+presskit.start({
+  entryPoint: program.args[0],
+  watch: program.watch
+})
---
+++ bin/presskit-new.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const chalk = require('chalk')
+const program = require('commander')
+const presskit = require('../lib/index')
+
+// -------------------------------------------------------------
+// Constants.
+// -------------------------------------------------------------
+
+const version = require('../package.json').version
+
+const usage = chalk.green('[options]') + ' ' + chalk.yellow('<destination>')
+
+const description = `Create an empty \`data.xml\` file and its \`images/\` folder in the <destination> folder (current working directory by default).
+
+  There are two template types available: ${chalk.blue('company')} (default) or ${chalk.blue('product')}.`
+
+// -------------------------------------------------------------
+// Module.
+// -------------------------------------------------------------
+
+program
+  .version(version)
+  .description(description)
+  .usage(usage)
+  .option('-t, --type [company]', 'Set the type of the new `data.xml` file', 'company')
+  .parse(process.argv)
+
+presskit.template(program.type, program.args[0] || process.cwd())
